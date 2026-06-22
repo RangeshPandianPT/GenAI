79ec105dc695e6c043f8acca642886a000208e4a
RangeshPandianPT <rangeshpandian@gmail.com>
2026-06-22 21:19:28 +0530

    feat(finance-agent): add initial agent implementation

---
+++ ADK_FINANCE_ASSISTANCE_AGENT/finance_assistant_agent/agent.py
@@ -0,0 +1,13 @@
+from google.adk.agents import LlmAgent
+
+finance_assistance_agent = LlmAgent(
+    name="finance_assistance_agent",
+    model="gemini-2.5-flash",
+    description="A simple finance assistant that helps with user's finance goals.",
+    instruction="""You are a friendly finance assistant.
+        You can help answer user's generic questions on finance and help plan
+        their finance goals. Be more friendly and positive.
+    """,
+)
+
+root_agent = finance_assistance_agent
